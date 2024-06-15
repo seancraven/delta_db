a23e89626577ac9c76bfddaf5b33e7d271bc55c1
sean craven <seanpietercraven@gmail.com>
2024-06-15 12:03:58 +0100

    feat: make ui have nice interface.

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,39 +1,59 @@
+pub mod matcher;
 pub mod tui;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    layout::Alignment,
+    layout::{self, Alignment, Constraint, Direction, Layout},
     style::Stylize,
-    symbols::border,
-    text::Line,
+    text::{Line, Text},
     widgets::{
         block::{Position, Title},
-        Block, Borders, Paragraph, Widget,
+        Block, Borders, Padding, Paragraph, Widget,
     },
     Frame,
 };
 use std::io::{self, Result};
+use tui::restore;
 fn main() -> Result<()> {
     let mut t = tui::init()?;
-    let mut a = App::default();
+    let mut a = App::new();
     a.run(&mut t)?;
+    restore()?;
     Ok(())
 }
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 struct App {
-    string: String,
-    exit: bool,
+    input_box: InputTextbox,
+    search_results: Resutlts,
+    layout: Layout,
 }
 impl App {
+    pub fn new() -> App {
+        let layout = layout::Layout::new(
+            Direction::Vertical,
+            [Constraint::Percentage(100), Constraint::Min(3)],
+        );
+        let input_box = InputTextbox::default();
+        let search_results = Resutlts {
+            r: vec!["Test".into(), "Testy".into()],
+        };
+        App {
+            layout,
+            search_results,
+            input_box,
+        }
+    }
     pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-        while !self.exit {
+        while !self.input_box.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
             self.handle_events()?;
         }
         Ok(())
     }
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
+        let l = self.layout.split(frame.size());
+        frame.render_widget(&self.search_results, l[0]);
+        frame.render_widget(&self.input_box, l[1]);
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -45,19 +65,24 @@ impl App {
     }
     fn handle_key_events(&mut self, k: KeyEvent) {
         match k.code {
-            KeyCode::Char(c) => self.string.push(c),
+            KeyCode::Char(c) => self.input_box.string.push(c),
             KeyCode::Backspace => {
-                self.string.pop();
+                self.input_box.string.pop();
             }
             KeyCode::Esc => {
-                self.exit = true;
+                self.input_box.exit = true;
             }
             _ => (),
         }
     }
 }
+#[derive(Default, Debug)]
+struct InputTextbox {
+    string: String,
+    exit: bool,
+}
 
-impl Widget for &App {
+impl Widget for &InputTextbox {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
@@ -71,11 +96,30 @@ impl Widget for &App {
                     .alignment(Alignment::Center)
                     .position(Position::Bottom),
             )
-            .borders(Borders::all())
-            .border_set(border::THICK);
-        Paragraph::new(&*self.string)
+            .borders(Borders::LEFT | Borders::RIGHT | Borders::BOTTOM)
+            .border_type(ratatui::widgets::BorderType::Rounded);
+        Paragraph::new(format!("❯  {}", &*self.string))
             .left_aligned()
             .block(block)
             .render(area, buf);
+    }
+}
+#[derive(Debug, Default)]
+struct Resutlts {
+    r: Vec<String>,
+}
+impl Widget for &Resutlts {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        let text_block = Text::from_iter(self.r.iter().map(|line| Line::from(line.as_str())))
+            .alignment(Alignment::Left);
+        let y_off = area.height - text_block.height() as u16;
+        let block = Block::default()
+            .borders(Borders::LEFT | Borders::RIGHT | Borders::TOP)
+            .border_type(ratatui::widgets::BorderType::Rounded)
+            .padding(Padding::new(0, 0, y_off - 1, 0));
+        Paragraph::new(text_block).block(block).render(area, buf);
     }
 }
