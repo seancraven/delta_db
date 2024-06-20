518e567e92a67411b0ed11f5876e708f0fe15516
sean craven <seanpietercraven@gmail.com>
2024-06-20 19:08:46 +0100

    feat: tui matcher done!

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,52 +1,74 @@
 pub mod matcher;
 pub mod tui;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use matcher::Matcher;
 use ratatui::{
     layout::{self, Alignment, Constraint, Direction, Layout},
-    style::Stylize,
-    text::{Line, Text},
+    style::{Color, Style, Stylize},
+    text::{Line, Span, Text},
     widgets::{
         block::{Position, Title},
         Block, Borders, Padding, Paragraph, Widget,
     },
     Frame,
 };
-use std::io::{self, Result};
+use std::{
+    fs::{read_to_string, File},
+    io::{self, Read, Result},
+};
 use tui::restore;
 fn main() -> Result<()> {
     let mut t = tui::init()?;
-    let mut a = App::new();
+    let f = read_to_string("src/tui.rs")?
+        .lines()
+        .map(String::from)
+        .collect();
+    let mut a = App::new(f);
     a.run(&mut t)?;
     restore()?;
     Ok(())
 }
 
-#[derive(Debug)]
 struct App {
     input_box: InputTextbox,
     search_results: Resutlts,
     layout: Layout,
+    matcher: Matcher,
 }
 impl App {
-    pub fn new() -> App {
+    pub fn new(inital_search_values: Vec<String>) -> App {
         let layout = layout::Layout::new(
             Direction::Vertical,
             [Constraint::Percentage(100), Constraint::Min(3)],
         );
         let input_box = InputTextbox::default();
         let search_results = Resutlts {
-            r: vec!["Test".into(), "Testy".into()],
+            r: vec![],
+            column_matches: vec![],
         };
+        let mut matcher = Matcher::new();
+        matcher.add_new_strings(inital_search_values);
         App {
             layout,
             search_results,
             input_box,
+            matcher,
         }
     }
     pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
         while !self.input_box.exit {
+            self.matcher.tick();
+            let mut string_results = vec![];
+            let mut columnt_results = vec![];
+            for (string, column_) in self.matcher.get_matches() {
+                string_results.push(string);
+                columnt_results.push(column_);
+            }
+            self.search_results.r = string_results;
+            self.search_results.column_matches = columnt_results;
             terminal.draw(|frame| self.render_frame(frame))?;
             self.handle_events()?;
+            self.matcher.add_target(self.input_box.string.as_str());
         }
         Ok(())
     }
@@ -98,28 +120,49 @@ impl Widget for &InputTextbox {
             )
             .borders(Borders::LEFT | Borders::RIGHT | Borders::BOTTOM)
             .border_type(ratatui::widgets::BorderType::Rounded);
-        Paragraph::new(format!("❯  {}", &*self.string))
-            .left_aligned()
-            .block(block)
-            .render(area, buf);
+        Paragraph::new(Line::from(vec![
+            Span::styled("❯  ", Style::default().fg(Color::Green)),
+            Span::from(&*self.string),
+        ]))
+        .left_aligned()
+        .block(block)
+        .render(area, buf);
     }
 }
 #[derive(Debug, Default)]
 struct Resutlts {
     r: Vec<String>,
+    column_matches: Vec<Vec<u32>>,
 }
 impl Widget for &Resutlts {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
-        let text_block = Text::from_iter(self.r.iter().map(|line| Line::from(line.as_str())))
-            .alignment(Alignment::Left);
+        let text_block = Text::from_iter(
+            self.r
+                .iter()
+                .zip(self.column_matches.iter())
+                .map(|(l, i)| self.format_line(l, i)),
+        )
+        .alignment(Alignment::Left);
         let y_off = area.height - text_block.height() as u16;
         let block = Block::default()
             .borders(Borders::LEFT | Borders::RIGHT | Borders::TOP)
             .border_type(ratatui::widgets::BorderType::Rounded)
             .padding(Padding::new(0, 0, y_off - 1, 0));
         Paragraph::new(text_block).block(block).render(area, buf);
+    }
+}
+impl Resutlts {
+    fn format_line(&self, l: &str, indecies: &Vec<u32>) -> Line {
+        Line::from(
+            l.char_indices()
+                .map(|(i, c)| match indecies.contains(&(i as u32)) {
+                    true => Span::styled(c.to_string(), Style::new().fg(Color::Green)),
+                    false => Span::raw(c.to_string()),
+                })
+                .collect::<Vec<Span>>(),
+        )
     }
 }
--- tui/src/matcher.rs
+++ tui/src/matcher.rs
@@ -1,18 +1,34 @@
 use std::sync::Arc;
 
-use nucleo::{self, Config, Nucleo, Utf32String};
+use nucleo::{
+    self,
+    pattern::{CaseMatching, MultiPattern, Normalization, Pattern},
+    Config, Nucleo, Utf32String,
+};
 
 pub struct Matcher {
     engine: Nucleo<String>,
 }
 impl Matcher {
-    fn new() -> Matcher {
+    pub fn new() -> Matcher {
         Matcher {
             engine: Nucleo::new(Config::DEFAULT, Arc::new(|| {}), Some(2), 1),
         }
     }
+    pub fn add_target(&mut self, target: impl AsRef<str>) {
+        self.engine.pattern.reparse(
+            0,
+            target.as_ref(),
+            CaseMatching::Smart,
+            Normalization::Smart,
+            false,
+        )
+    }
+    pub fn get_target(&self) -> &MultiPattern {
+        &self.engine.pattern
+    }
 
-    fn add_new_strings(&mut self, strings: Vec<String>) {
+    pub fn add_new_strings(&mut self, strings: Vec<String>) {
         let injector = self.engine.injector();
         for string in strings {
             injector.push(string.clone(), |s, asci_str| {
@@ -20,12 +36,74 @@ impl Matcher {
             });
         }
     }
-    fn get_matches(&self) {
-        let it = self
-            .engine
+    pub fn tick(&mut self) {
+        self.engine.tick(10);
+    }
+    pub fn get_matches(&self) -> Vec<(String, Vec<u32>)> {
+        let max = self.engine.snapshot().matched_item_count();
+        let mut m = nucleo::Matcher::new(Config::DEFAULT);
+        let indexer = self.engine.snapshot().pattern().column_pattern(0);
+        self.engine
             .snapshot()
-            .matched_items(0..30)
-            .map(|f| f.data.clone())
-            .collect::<Vec<String>>();
+            .matched_items(0..max)
+            .map(|f| {
+                (
+                    f.data.clone(),
+                    get_indeceis(f.matcher_columns[0].clone(), &mut m, indexer),
+                )
+            })
+            .rev()
+            .collect::<Vec<(String, Vec<u32>)>>()
+    }
+}
+fn get_indeceis(a: Utf32String, m: &mut nucleo::Matcher, i: &Pattern) -> Vec<u32> {
+    let mut out = vec![];
+    i.indices(a.slice(..), m, &mut out);
+    out.sort_unstable();
+    out.dedup();
+    out
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_match_all() {
+        let mut m = Matcher::new();
+        let t: Vec<String> = vec!["test", "tester", "testest"]
+            .into_iter()
+            .map(|s| s.to_string())
+            .collect();
+        m.add_target("tes");
+        m.add_new_strings(t.clone());
+        m.tick();
+        let matches = m.get_matches();
+        for match_ in matches {
+            assert!(t.contains(&match_.0))
+        }
+    }
+    #[test]
+    fn test_match_one() {
+        let mut m = Matcher::new();
+        let t: Vec<String> = vec!["test", "tester", "testest"]
+            .into_iter()
+            .map(|s| s.to_string())
+            .collect();
+        m.add_target("tester");
+        m.add_new_strings(t.clone());
+        m.tick();
+        assert_eq!(m.get_matches()[0].0, t[1]);
+    }
+    #[test]
+    fn test_find_none() {
+        let mut m = Matcher::new();
+        let t: Vec<String> = vec!["test", "tester", "testest"]
+            .into_iter()
+            .map(|s| s.to_string())
+            .collect();
+        m.add_target("dave");
+        m.add_new_strings(t.clone());
+        m.tick();
+        assert!(m.get_matches().len() == 0, "{:?}", m.get_matches().len());
     }
 }
